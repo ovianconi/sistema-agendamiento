f47b1b67ac39b2293cf32e5bab1db6e0613eac58
ovianconi <ovianconi@gmail.com>
2025-03-16 16:51:36 -0300

    Primera versión del sistema de agendamiento

---
+++ src/test/java/com/estetica/agendamiento/SgacerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estetica.agendamiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgacerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
